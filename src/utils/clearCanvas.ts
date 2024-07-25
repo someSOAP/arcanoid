aec256d63286f3fd55b0ee680176fe5f8e7f0810
someSOAP <andrey.meg@gmail.com>
2024-07-25 17:25:55 +0500

    transparent canvas

--- src/utils/clearCanvas.ts
+++ src/utils/clearCanvas.ts
@@ -1,8 +1,3 @@
-import space from '@/sprites/space.jpg'
-
-const background = new Image()
-background.src = space
-
 function clearCanvas(canvas: HTMLCanvasElement): void {
   const context = canvas.getContext('2d')
 
@@ -10,7 +5,7 @@ function clearCanvas(canvas: HTMLCanvasElement): void {
     throw new Error('Canvas has no 2D context')
   }
 
-  context.drawImage(background, 0, 0, canvas.width, canvas.height)
+  context.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 export default clearCanvas
